be858ad086de6ff4da1cafa1d69c8cb9c292f394
berrek <bartoszbehring@gmail.com>
2018-03-07 20:43:09 +0100

    Example to show the issue

---
+++ JsonSerializationBlockingCalls/JsonSerializationBlockingCalls/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using JsonSerializationBlockingCalls.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JsonSerializationBlockingCalls.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        [Route("{size}")]
+        public TestResponse Get(int size)
+        {
+            return TestResponse.TestValues[size];
+        }
+    }
+}
